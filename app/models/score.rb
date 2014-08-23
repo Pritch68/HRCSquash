77e3480f0f253e3878aabb86cddc3e684681e285
Pritch68 <terry.pritchard@rogers.com>
2014-08-23 21:10:55 +0000

    Created relationship between Users and Scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,6 @@
 class Score < ActiveRecord::Base
+
+  belongs_to :homeplayer, :class_name => "User", :foreign_key => "player1_id"
+	belongs_to :visitingplayer, :class_name => "User", :foreign_key => "player2_id"
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates_presence_of :name
+	validates_uniqueness_of :name, :email, :case_sensitive => false
+  
+  has_many :homescores, :class_name => "Score", :foreign_key => "player1_id"
+	has_many :visitingscores, :class_name => "Score", :foreign_key => "player2_id"
+  
 end
