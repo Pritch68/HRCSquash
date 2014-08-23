c4975b94437843003a192d268c37895473a2b4eb
Pritch68 <terry.pritchard@rogers.com>
2014-08-23 21:17:45 +0000

    Added scopes and stats method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,15 @@ class User < ActiveRecord::Base
   
   has_many :homescores, :class_name => "Score", :foreign_key => "player1_id"
 	has_many :visitingscores, :class_name => "Score", :foreign_key => "player2_id"
+    	
+	scope :ranked, order("users.points DESC, users.name")
+	scope :active, where("users.deleted = false")
+	scope :possible_opponents, lambda {|uid| select("id,name").where("id != ?", uid).order("name")}
+	
+	def self.stats pid
+		total_matches = Score.where("player1_id = ? or player2_id = ?",pid,pid).count
+		wins = Score.where("player1_id = ? and matchscore < 3",pid).count + Score.where("player2_id = ? and matchscore > 2",pid).count
+		return { :matches => total_matches, :wins => wins }
+	end
   
 end
