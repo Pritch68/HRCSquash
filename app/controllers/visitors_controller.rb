e3363fd36fafc1bbb2f485ba4676c9a38f74b5a0
Pritch68 <terry.pritchard@rogers.com>
2014-08-24 17:23:02 +0000

    Added Deleted to Users

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+  
+  def index
+    # @users = User.active.ranked.limit(10)
+  	# @frontscores = Score.recent.limit(10)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823210444) do
+ActiveRecord::Schema.define(version: 20140824172159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140823210444) do
     t.string   "name"
     t.integer  "points"
     t.boolean  "admin"
+    t.boolean  "deleted"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
