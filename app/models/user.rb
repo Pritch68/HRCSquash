e2ac57a1fe45cfe0ff43cbd443d53340d594c6ed
Pritch68 <terry.pritchard@rogers.com>
2014-09-24 02:19:57 +0000

    added default values to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  after_initialize :defaults
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
@@ -15,6 +18,13 @@ class User < ActiveRecord::Base
   scope :active, -> { where("deleted = false") }
 	scope :possible_opponents, lambda {|uid| select("id,name").where("id != ?", uid).order("name")}
 	
+  def defaults
+    self.points ||= 1000
+    self.lastchange ||= 0
+    self.admin = false if self.admin.nil?
+    self.deleted = false if self.deleted.nil?
+  end
+  
 	def self.stats pid
 		total_matches = Score.where("player1_id = ? or player2_id = ?",pid,pid).count
 		wins = Score.where("player1_id = ? and matchscore < 3",pid).count + Score.where("player2_id = ? and matchscore > 2",pid).count
